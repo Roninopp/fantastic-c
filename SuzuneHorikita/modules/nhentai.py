ca2767fc71be6565f40cab5f778320bb3a9b283c
Arsh <87128542+ArshCypherZ@users.noreply.github.com>
2022-06-11 20:27:38 +0530

    Update nhentai.py

--- SuzuneHorikita/modules/nhentai.py
+++ SuzuneHorikita/modules/nhentai.py
@@ -12,7 +12,7 @@ from pyrogram.types import (InlineKeyboardMarkup,
 
                             )
 
-from SuzuneHorikita import pgram, Telegraph
+from SuzuneHorikita import pgram
 
 from SuzuneHorikita.utils.errors import capture_err
 
