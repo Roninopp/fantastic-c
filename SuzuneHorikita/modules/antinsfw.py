e6d6cfb7e0b5a9a470566fd0f0a62680f497c6b9
Arsh <87128542+ArshCypherZ@users.noreply.github.com>
2022-06-14 11:38:20 +0530

    Update antinsfw.py

--- SuzuneHorikita/modules/antinsfw.py
+++ SuzuneHorikita/modules/antinsfw.py
@@ -256,35 +256,19 @@ async def nsfw_scan_command(_, message):
 
 @adminsOnly("can_change_info")
 async def nsfw_enable_disable(_, message):
-
     if len(message.command) != 2:
-
         await message.reply_text("Usage: /antinsfw [on/off]")
-
         return
-
     status = message.text.split(None, 1)[1].strip()
-
     status = status.lower()
-
     chat_id = message.chat.id
-
     if status == "on" or status == "yes":
-
         await nsfw_on(chat_id)
-
-        await message.reply_text(
-
-            "Enabled AntiNSFW System. I will Delete Messages Containing Inappropriate Content."
-
-        )
-
+        await message.reply_text("Enabled AntiNSFW System. I will Delete Messages Containing Inappropriate Content.")
+        
     elif status == "off" or status == "no":
-
         await nsfw_off(chat_id)
-
         await message.reply_text("Disabled AntiNSFW System.")
-
+        
     else:
-
         await message.reply_text("Unknown Suffix, Use /antinsfw [on/off]")
