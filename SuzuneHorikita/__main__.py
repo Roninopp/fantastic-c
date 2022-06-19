1127ac26af0027a862b372cebd47a1e7714d56dd
Rin Okumura <101918125+Kanekiken099999@users.noreply.github.com>
2022-06-19 19:47:02 +0530

    Update __main__.py

--- SuzuneHorikita/__main__.py
+++ SuzuneHorikita/__main__.py
@@ -229,8 +229,8 @@ def start(update: Update, context: CallbackContext):
         else:
             first_name = update.effective_user.first_name
             update.effective_message.reply_photo(
-                PM_START_TEXT, 
                 SuzuneHorikitarobot_IMG, 
+                PM_START_TEXT, 
                 reply_markup=InlineKeyboardMarkup(buttons),
                 parse_mode=ParseMode.MARKDOWN,
                 timeout=60,
