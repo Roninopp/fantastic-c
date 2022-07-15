21905e5f4d0471a0f2e651f1bb0c6d7ddbea4bf8
Arsh <87128542+ArshCypherZ@users.noreply.github.com>
2022-07-15 10:40:07 +0530

    Update music.py

--- SuzuneHorikita/modules/music.py
+++ SuzuneHorikita/modules/music.py
@@ -48,7 +48,7 @@ def download_youtube_audio(arq_resp):
     return [title, performer, duration, audio_file, thumbnail_file]
 
 
-@pgram.on_message(filters.command("ytmusic") & ~filters.edited)
+@pgram.on_message(filters.command("song") & ~filters.edited)
 @capture_err
 async def music(_, message):
     global is_downloading
@@ -103,73 +103,3 @@ async def download_song(url):
     song = BytesIO(song)
     song.name = "a.mp3"
     return song
-
-
-# Jiosaavn Music
-
-
-@pgram.on_message(filters.command("song") & ~filters.edited)
-@capture_err
-async def jssong(_, message):
-    global is_downloading
-    if len(message.command) < 2:
-        return await message.reply_text("/song requires an argument.")
-    if is_downloading:
-        return await message.reply_text(
-            "Another download is in progress, try again after sometime."
-        )
-    is_downloading = True
-    text = message.text.split(None, 1)[1]
-    m = await message.reply_text("Searching...")
-    try:
-        songs = await arq.saavn(text)
-        if not songs.ok:
-            await m.edit(songs.result)
-            is_downloading = False
-            return
-        sname = songs.result[0].song
-        slink = songs.result[0].media_url
-        ssingers = songs.result[0].singers
-        sduration = songs.result[0].duration
-        await m.edit("Downloading")
-        song = await download_song(slink)
-        await m.edit("Uploading")
-        await message.reply_audio(
-            audio=song,
-            title=sname,
-            performer=ssingers,
-            duration=sduration,
-        )
-        await m.delete()
-    except Exception as e:
-        is_downloading = False
-        return await m.edit(str(e))
-    is_downloading = False
-    song.close()
-
-
-# Lyrics
-
-
-@pgram.on_message(filters.command("lyrics") & ~filters.edited)
-async def lyrics_func(_, message):
-    if len(message.command) < 2:
-        return await message.reply_text("**Usage:**\n/lyrics [QUERY]")
-    m = await message.reply_text("**Searching**")
-    query = message.text.strip().split(None, 1)[1]
-
-    resp = await arq.lyrics(query)
-
-    if not (resp.ok and resp.result):
-        return await m.edit("No lyrics found.")
-
-    song = resp.result[0]
-    song_name = song['song']
-    artist = song['artist']
-    lyrics = song['lyrics']
-    msg = f"**{song_name}** | **{artist}**\n\n__{lyrics}__"
-          
-    if len(msg) > 4095:
-        msg = await paste(msg)
-        msg = f"**LYRICS_TOO_LONG:** [URL]({msg})"
-    return await m.edit(msg)
