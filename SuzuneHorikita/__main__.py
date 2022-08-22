6d14ca68125dcc3b464406a1cba20b7c9a021397
SOME1HING <yashprakash2005@gmail.com>
2022-08-22 14:51:33 +0530

    Update __main__.py

--- SuzuneHorikita/__main__.py
+++ SuzuneHorikita/__main__.py
@@ -232,6 +232,7 @@ def start(update: Update, context: CallbackContext):
             first_name = update.effective_user.first_name
             update.effective_message.reply_text(             
                 PM_START_TEXT, 
+                disable_web_page_preview=False,
                 reply_markup=InlineKeyboardMarkup(buttons),
                 parse_mode=ParseMode.MARKDOWN,
                 timeout=60,
