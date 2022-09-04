04f29b3df8ecbe97b4836cdf36117698d02727a2
Rin Okumura <101918125+Kanekiken099999@users.noreply.github.com>
2022-09-04 20:45:55 +0530

    Update dbfunctions.py

--- SuzuneHorikita/ex_plugins/dbfunctions.py
+++ SuzuneHorikita/ex_plugins/dbfunctions.py
@@ -3,7 +3,7 @@ import pickle
 from typing import Dict, List, Union
 
 from SuzuneHorikita.mongo import db
-from SuzuneHorikita import log
+from SuzuneHorikita import logger as log
 
 # SOME THINGS ARE FUCKED UP HERE, LIKE TOGGLEABLES HAVE THEIR OWN COLLECTION
 # (SHOULD FIX IT WITH SOMETHING LIKE TOGGLEDB), MOST OF THE CODE IS BAD AF
