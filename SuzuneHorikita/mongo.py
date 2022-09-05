e9e33636b0599442a4ca92ae4a9c6618f861de61
Yash-Sharma-1807 <98401272+Yash-Sharma-1807@users.noreply.github.com>
2022-09-05 23:57:16 +0530

    Update mongo.py

--- SuzuneHorikita/mongo.py
+++ SuzuneHorikita/mongo.py
@@ -1,24 +1,36 @@
-import asyncio
-import sys
-from SuzuneHorikita import LOGGER as log 
-from motor import motor_asyncio
-from pymongo import MongoClient
-from pymongo.errors import ServerSelectionTimeoutError
-from SuzuneHorikita import MONGO_DB_URI
-from SuzuneHorikita.confing import get_int_key, get_str_key
+
+import asyncio 
+
+ import sys 
+
+ from motor import motor_asyncio 
+
+ from pymongo import MongoClient 
+
+ from pymongo.errors import ServerSelectionTimeoutError 
+
+ from SuzuneHorikita.confing import get_int_key, get_str_key 
+
+ from SuzuneHorikita import LOGGER 
+
+  
+
+ client = MongoClient("mongodb+srv://ub:ub123@horivc.cemtd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority") 
+
+ client = MongoClient("mongodb+srv://ub:ub123@horivc.cemtd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", 27017)["SuzuneHorikita"] 
+
+ motor = motor_asyncio.AsyncIOMotorClient("mongodb+srv://ub:ub123@horivc.cemtd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", 27017) 
+
+ db = motor["SuzuneHorikita"] 
+
+ db = client["SuzuneHorikita"] 
+
+ try: 
+
+     asyncio.get_event_loop().run_until_complete(motor.server_info()) 
+
+ except ServerSelectionTimeoutError: 
+
+     sys.exit(LOGGER.critical("ERROR"))
 
 
-MONGO_PORT = get_int_key("27017")
-MONGO_DB_URI = get_str_key("mongodb+srv://ub:ub123@horivc.cemtd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
-MONGO_DB = "SuzuneHorikita"
-
-
-client = MongoClient()
-client = MongoClient(MONGO_DB_URI, MONGO_PORT)[MONGO_DB]
-motor = motor_asyncio.AsyncIOMotorClient(MONGO_DB_URI, MONGO_PORT)
-db = motor[MONGO_DB]
-db = client["emiexrobot"]
-try:
-    asyncio.get_event_loop().run_until_complete(motor.server_info())
-except ServerSelectionTimeoutError:
-    sys.exit(log.critical("Can't connect to mongodb! Exiting..."))
